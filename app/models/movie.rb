c95a11c472c255ec624cea73920c9c25126d6982
Jonathan Bahena <bahena@Jonathans-MacBook-Air.local>
2021-07-07 18:05:22 -0500

    adds home

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,6 @@ class Movie < ApplicationRecord
   validates :year, numericality: {greater_than: 1887}
   validates :title, presence: true
   has_many :actors
+  has_many :movie_genres
+  has_many :genres, through: :movie_genres
 end
